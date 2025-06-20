3bb74e388bbaccfc099024cc7158ed91e3dcd1fc
wcode0 <westonlalonde@gmail.com>
2025-06-19 23:13:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let x = 0;
 let y = 0;
+let enemyx = 89;
 const speed = 0.5; 
 const player = document.getElementById("player");
 const enemy = document.getElementById("enemy");
+const sword = document.getElementById("sword");
 let keys = {
-  ArrowUp: false,
-  ArrowDown: false,
-  ArrowLeft: false,
-  ArrowRight: false
+  KeyW: false,  // was ArrowUp
+  KeyS: false,  // was ArrowDown
+  KeyA: false,  // was ArrowLeft
+  KeyD: false,   // was ArrowRight
+  ArrowRight: false,
+  ArrowLeft : false
 };
+window.addEventListener('click', () => {
+  location.reload();
+});
 
 // Listen for key presses
 window.addEventListener("keydown", (event) => {
@@ -23,17 +30,18 @@ window.addEventListener("keyup", (event) => {
     keys[event.code] = false;
   }
 });
-
+let enemyspeed =0.25
 let grav = 0.2
 let velocity = 0
 let strength = -5
 let isJumping = true
+let swordAngle = 0
 const ground = 80
 
 function update() {
-
+  enemyx+=enemyspeed;
   player.style.height = "10vw";
-  if (keys.ArrowUp && !isJumping){
+  if (keys.KeyW && !isJumping){
     velocity = strength
     isJumping = true
   }
@@ -47,12 +55,27 @@ function update() {
     isJumping = false;
   }
   
-  if (keys.ArrowLeft) x -= speed;
-  if (keys.ArrowRight) x += speed;
-  if (keys.ArrowDown) player.style.height = "5vw";
+  if (keys.KeyA && x > 0) x -= speed;
+  if (keys.KeyD && x < 90) x += speed;
+  if (keys.KeyS) player.style.height = "5vw";
+  //enemy movement
+  if(enemyx <= 0 || enemyx >= 90){ 
+    enemyspeed*=-1}
+  //woah that was easier than I thought
+  //sword moving time!!!!!!
+  sword.style.left = x + 3 + "vw"
+  sword.style.top = y - 25 + "vh"
+  //sword rotation
+  if(keys.ArrowLeft){
+    swordAngle -=1
+  }
+  if(keys.ArrowRight){
+    swordAngle+=1;
+  }
+  sword.style.rotate = swordAngle + "deg"
   player.style.left = x + "vw";
   player.style.top = y + "vh";
-
+  enemy.style.left = enemyx + "vw";
   requestAnimationFrame(update);
 }
 
